b6e239703d8b557f683d0ea3cc9cc02c4d51290b
Priyansh Jain <priyanshjain7067@gmail.com>
2026-01-20 18:18:52 +0530

    changing envs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 main()
   .then(() => {
